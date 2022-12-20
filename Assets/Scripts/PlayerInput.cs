15b2cd39f45e9ea02cc5ba5b25ce0d59fb4f1501
KingOneChance <wonchan32@gmail.com>
2022-12-20 16:15:55 +0900

    Puzzle
    Present

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public delegate void Del_PlayerClick(string dir);
+    public Del_PlayerClick del_PlayerClick;
+    public bool canClick = true;
     void Update()
     {
-        
+        if (canClick==false) return;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            del_PlayerClick("Left");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            del_PlayerClick("Right");
+        }
     }
 }
--- Assets/Scripts/PresentInfo.cs
+++ Assets/Scripts/PresentInfo.cs
@@ -15,8 +15,13 @@ public class PresentInfo : MonoBehaviour
     [SerializeField] private GameObject leftPos;
     [SerializeField] private GameObject rightPos;
     [SerializeField] private GameObject targetPos;
+    [SerializeField] private PlayerInput PlayerInput;
+    [SerializeField] private Rigidbody rigidbody;
     private void Start()
     {
+        rigidbody = GetComponent<Rigidbody>();
+        PlayerInput = FindObjectOfType<PlayerInput>();
+        PlayerInput.del_PlayerClick = PresentDir;
         if (gameObject.tag == "Present") state = PresentState.Present;
         else if (gameObject.tag == "Bomb") state = PresentState.Bomb;
         else Debug.Log("This Object don't have any tag.");
@@ -32,11 +37,12 @@ public class PresentInfo : MonoBehaviour
     //CheckPoint의 PlayerInput Script에서 호출 
     public void PresentDir(string dir)
     {
-        if (dir == "Right") targetPos = leftPos;
-        else if (dir == "Left") targetPos = rightPos;
+        if (dir == "Right") targetPos = rightPos;
+        else if (dir == "Left") targetPos = leftPos;
         else Debug.Log("It is not selected value ");
         PresentMove();
     }
+
     public void PresentMove()
     {
         if (canMove == true)
@@ -44,7 +50,9 @@ public class PresentInfo : MonoBehaviour
             //상태가 선물일 경우 
             if (state == PresentState.Present)
             {
-                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPos.transform.position, 0.01f * moveSpeed);
+                Debug.Log("?");
+                gameObject.transform.LookAt(targetPos.transform);
+                StartCoroutine(Co_PresentMove());
                 if (targetPos == rightPos)
                 {
                     //UI 점수 올려주는 함수 호출 
@@ -54,13 +62,31 @@ public class PresentInfo : MonoBehaviour
             //상태가 폭탄일 경우
             else if (state == PresentState.Bomb)
             {
+                Debug.Log("?");
+                gameObject.transform.LookAt(targetPos.transform);
+                StartCoroutine(Co_PresentMove());
                 //이동 시키기 
-                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPos.transform.position, 0.01f * moveSpeed);
                 if (targetPos == rightPos)
                 {
-                    //일시적으로 키입력 막는 로직 작성
+                    //일시적으로 키입력 막는 로직
+                    PlayerInput.canClick = false;
+                    Invoke("PauseCancle", 1);
                 }
             }
         }
     }
+    IEnumerator Co_PresentMove()
+    {
+        int i = 0;
+        while (i < 100)
+        {
+            rigidbody.transform.Translate(Vector3.forward * 0.1f);
+            i++;
+            yield return null;
+        }
+        canMove = false;
+        yield return null;
+    }
+    private void PauseCancle() => PlayerInput.canClick = true;
+
 }
